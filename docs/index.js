4930cd245074109ed4a8a760ed03586d347d74a0
todverse <todverse0@gmail.com>
2021-11-19 18:40:50 +0400

    add portfolio

---
+++ docs/index.js
@@ -0,0 +1,5 @@
+let work1 = document.getElementsByClassName('work')[0];
+let mainFlex = document.getElementsByClassName('mainFlex__work')[0];
+work1.addEventListener('click', function(event) {
+	
+});
